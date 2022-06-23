32613673c5488289371050642bf3136d1ecd8988
professorlozano <luizmlozano@gmail.com>
2022-06-23 15:36:05 -0300

    projeto api encontro remoto 1

--- UC11/ER1/Chapter/Chapter/Program.cs
+++ UC11/ER1/Chapter/Chapter/Program.cs
@@ -1,8 +1,15 @@
+using Chapter.Contexts;
+using Chapter.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+builder.Services.AddTransient<LivroRepository, LivroRepository>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
