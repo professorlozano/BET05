650f9ca4b085a3fae35b502d1d146441182a2b5d
professorlozano <luizmlozano@gmail.com>
2022-07-21 21:01:49 -0300

    projeto do encontro remoto 3 de api

---
+++ UC11/ER3/Chapter/Chapter/Controllers/UsuarioController.cs
@@ -0,0 +1,93 @@
+﻿
+using Chapter.Interfaces;
+using Chapter.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chapter.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository; 
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                return Ok(_usuarioRepository.Listar());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+                Usuario usuarioEncontrado = _usuarioRepository.BuscarPorId(id);
+                if(usuarioEncontrado == null)
+                {
+                    return NotFound();
+                }
+                return Ok(usuarioEncontrado);
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            try
+            {
+                _usuarioRepository.Cadastrar(usuario);
+                return StatusCode(201);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Alterar (int id, Usuario usuario)
+        {
+            try
+            {
+                _usuarioRepository.Atualizar(id, usuario);
+                return Ok("Usuario Alterado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+                return Ok("Usuário Deletado");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
